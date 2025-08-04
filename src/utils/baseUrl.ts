270613c1e9d784ed6983bc2122002e6186169545
Gaurav Kashyap <gauravkashyap@Gauravs-MacBook-Air.local>
2025-08-04 14:00:34 +0530

    change the production url

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,3 +1,3 @@
 // export const baseUrl = 'http://localhost:9000/api/v1' //local
-export const baseUrl = 'https://dev.aameego.com/api/v1'; //dev
-// export const baseUrl = 'https://api.aameego.com/api/v1'; //production
+// export const baseUrl = 'https://dev.aameego.com/api/v1'; //dev
+export const baseUrl = 'https://api.aameego.com/api/v1'; //production
